57fd95cf2cb05310d1cbced004687984cd9d1ad4
prasadretar08 <63647174+prasadretar08@users.noreply.github.com>
2024-02-20 19:37:40 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Praents New Healty Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 * Blueberry Oatmeal')
+streamlit.text('Kale, Spniach & Rocket Snoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
