cacd7f06fe5af1cbb3c635842a0ee015a405f80e
prasadretar08 <63647174+prasadretar08@users.noreply.github.com>
2024-03-05 15:26:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import snowflake.connector
 from urllib.error import URLError 
 
 streamlit.title('My Praents New Healty Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu') 
 streamlit.text('Omega 3 * Blueberry Oatmeal')
 streamlit.text('Kale, Spniach & Rocket Snoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
